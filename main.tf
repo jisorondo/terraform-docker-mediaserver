20b6b403b2097367132f2564475acba31eb70f2c
jisorondo <jsorondo@hashicorp.com>
2021-10-18 20:02:39 -0500

    comment added regarding ssh

--- main.tf
+++ main.tf
@@ -8,7 +8,8 @@ terraform {
   }
 }
 
+// To deploy on a remote server uncomment the host below and replace IPAddress with host IP and port if needed. 
 provider "docker" {
-  // host = "ssh://root@node1:22"
+  // host = "ssh://root@IPAddress:22"
 
 }
