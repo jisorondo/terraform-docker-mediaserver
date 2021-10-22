ad6bac5e94737e6ee502f34a55cfddd7a4b9ddf4
jisorondo <jsorondo@hashicorp.com>
2021-10-22 12:15:24 -0500

    testing purposes

--- main.tf
+++ main.tf
@@ -10,6 +10,6 @@ terraform {
 
 // To deploy on a remote server uncomment the host below and replace IPAddress with host IP and port if needed. 
 provider "docker" {
-  // host = "ssh://root@IPAddress:22"
+  host = "ssh://root@node1:22"
 
 }
